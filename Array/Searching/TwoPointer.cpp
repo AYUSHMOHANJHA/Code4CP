c193d1c56e2ca7fcb9121e9c38f523ec40cd9705
AYUSHMOHANJHA <ayushjha06@gmail.com>
2020-09-28 23:25:12 +0530

    Two Pointer Basic

---
+++ Array/Searching/TwoPointer.cpp
@@ -0,0 +1,37 @@
+/*Two Pointers Technique*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int a [] = {1,2,3,4,5,6,7,8,9,10};
+    int s = sizeof(a)/sizeof(int);
+   
+    int X=0;
+    cin>>X;
+
+    int i = 0;
+    int j = s-1;
+
+    int flag = 0;
+
+    while(i<j){
+        //cout<<i<<"-"<<j<<endl;
+        if((a[i]+a[j])==X){
+            flag=1;
+            break;
+        }
+        else if((a[i]+a[j]) < X)
+            i++;
+        else
+            j--;
+    }
+        if(flag==0) 
+            cout<<"Not Found";
+        else 
+            cout<<"Found";
+
+    return 0;
+}
