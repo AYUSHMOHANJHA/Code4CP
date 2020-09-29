0def4dfa3275384cffabef1069ab79428131c7db
AYUSHMOHANJHA <ayushjha06@gmail.com>
2020-09-29 20:14:06 +0530

    Two Pointer Basic

--- Array/Searching/TwoPointer.cpp
+++ Array/Searching/TwoPointer.cpp
@@ -29,9 +29,9 @@ int main(){
             j--;
     }
         if(flag==0) 
-            cout<<"Not Found";
+            cout<<"Not Found"<<endl;
         else 
-            cout<<"Found";
+            cout<<"Found"<<endl;
 
     return 0;
 }
