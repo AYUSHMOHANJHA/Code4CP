780737f0c5c7e4360529b5993382efc2f30699a4
AYUSHMOHANJHA <ayushjha06@gmail.com>
2020-10-23 20:38:00 +0530

    DFS using vectors of vector

---
+++ Graph/DFS.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+void dfs(int start, vector<int>graph[], bool *visited){
+    visited[start] = true;
+    cout<<start<<" ";
+    for(int i=0; i<graph[start].size();++i){
+        if(visited[graph[start][i]]==false)
+            dfs(graph[start][i],graph,visited);
+    } 
+}
+int main(){
+
+    int N,E;
+    cin>>N>>E;
+
+    vector<int>graph[N];
+    bool visited[N];
+
+    memset(visited, false, sizeof(visited));
+
+    for(int i=0;i<E;i++){
+        int u,v;
+        cin>>u>>v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+    dfs(0,graph,visited);
+    cout<<endl;
+
+}
