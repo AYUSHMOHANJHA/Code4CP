a8efb5e9c1a8266c5866c41ae64acfd034aa0c90
AYUSHMOHANJHA <ayushjha06@gmail.com>
2020-12-17 19:19:39 +0530

    Codeforces code added

---
+++ _Online/Codeforces/EC99/a.cpp
@@ -0,0 +1,15 @@
+//https://codeforces.com/contest/1455/problem/A
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        cout<<s.size()<<endl;
+    }
+    return 0;
+}
