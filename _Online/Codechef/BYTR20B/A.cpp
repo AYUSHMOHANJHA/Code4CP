653de247e750bc0e47492820c525ea5a303eb296
AYUSHMOHANJHA <ayushjha06@gmail.com>
2020-12-29 22:22:29 +0530

    Codechef code added

---
+++ _Online/Codechef/BYTR20B/A.cpp
@@ -0,0 +1,24 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define mod 1000000007
+
+void sol(){
+    ll N,K;
+    cin>>N>>K;
+    cout<<K/N<<endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL); 
+    ll T;
+    cin>>T;
+    while(T--){
+        sol();
+    }
+    return 0;
+}
